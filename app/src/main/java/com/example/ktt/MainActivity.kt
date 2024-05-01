52ded7326966dfaf2a4b64f5cc60599c6a98561d
Yerson Borda <kira_yerson2407@hotmail.com>
2024-05-01 17:44:56 +0700

    Toasts

--- app/src/main/java/com/example/ktt/MainActivity.kt
+++ app/src/main/java/com/example/ktt/MainActivity.kt
@@ -4,22 +4,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -29,10 +16,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,81 +40,87 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class ColorInfo(
+    val name: String,
+    val color: Color,
+    val initial: String
+)
+
 @Composable
 fun Task() {
-    val rainbowColors = listOf("Red", "Orange", "Yellow", "Green", "Blue", "Dark Blue", "Violet")
+    val rainbowColors = listOf(
+        ColorInfo("Red", Color.Red, "R"),
+        ColorInfo("Orange", Color(0xFFff8c04), "O"),
+        ColorInfo("Yellow", Color.Yellow, "Y"),
+        ColorInfo("Green", Color.Green, "G"),
+        ColorInfo("Blue", Color.Blue, "B"),
+        ColorInfo("Dark Blue", Color(0xFF00008B), "D"),
+        ColorInfo("Violet", Color(0xFFa004fc), "V")
+    )
 
     val context = LocalContext.current
 
-    Column (
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(top = 20.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
         Text(
             text = "Select your favorite color!",
             fontWeight = FontWeight.Medium,
             fontSize = 20.sp
         )
 
-        LazyColumn (
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = 24.dp, vertical = 20.dp)
-        ){
-            items(rainbowColors){colors ->
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color(0xFFfff4fc)) //0xFFfff4fc
-                        .border(BorderStroke(1.dp, Color.Gray))
-                        .clip(shape = RoundedCornerShape(12.dp))
-                        .clickable {
-                            Toast.makeText(context, colors, Toast.LENGTH_SHORT).show()
-                        },
-                    contentAlignment = Alignment.Center
-                ){
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(start = 16.dp),
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        val colorPics = when (colors) {
-                            "Red" -> R.drawable.red
-                            "Orange" -> R.drawable.orange
-                            "Yellow" -> R.drawable.yellow
-                            "Green" -> R.drawable.green
-                            "Blue" -> R.drawable.blue
-                            "Dark Blue" -> R.drawable.darkblue
-                            "Violet" -> R.drawable.violet
-                            else -> null
-                        }
-
-                        colorPics?.let { painterResource(id = it) }?.let {
-                            Image(
-                                painter = it,
-                                contentDescription = null,
-                                modifier = Modifier.size(40.dp)
-
-                            )
-                        }
-
-                        Spacer(modifier = Modifier.width(16.dp))
-
-                        Text(
-                            text = colors,
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Medium
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(80.dp))
-                }
+        ) {
+            items(rainbowColors) { colorInfo ->
+                ColorBox(
+                    colorInfo = colorInfo,
+                    onClick = {
+                        Toast.makeText(context, colorInfo.name, Toast.LENGTH_SHORT).show()
+                    },
+                )
                 Spacer(modifier = Modifier.height(16.dp))
             }
         }
+    }
+}
+
+@Composable
+fun ColorBox(colorInfo: ColorInfo, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .background(Color(0xFFfff4fc), shape = RoundedCornerShape(15.dp))
+            .clickable(onClick = onClick)
+            .padding(start = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(64.dp)
+                .background(colorInfo.color, shape = RoundedCornerShape(32.dp)),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                text = colorInfo.initial,
+                fontSize = 24.sp,
+                color = Color.White
+            )
+        }
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Text(
+            text = colorInfo.name,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium
+        )
     }
 }
 
